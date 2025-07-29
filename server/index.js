c6d86ea331dc6f637a330bc9541a0b91a6c15224
Tehila Zlates <t3265137@gmail.com>
2025-07-29 16:09:06 +0300

    Updated server listen to use environment port

--- server/index.js
+++ server/index.js
@@ -126,6 +126,12 @@ app.use((err, req, res, next) => {
   res.status(500).send('Internal Server Error');
 });
 
-app.listen(3001, () => {
-  console.log('Server running at http://localhost:3001');
+// app.listen(3001, () => {
+//   console.log('Server running at http://localhost:3001');
+// });
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
+
