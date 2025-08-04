cf94c9516f09e68adac1ac382594ae352037ee77
Tehila Zlates <t3265137@gmail.com>
2025-08-05 00:48:24 +0300

    emailףף;;;ףףף'ף;ף;ף;

--- digital-sign-flow/src/components/SignPage/SignPage.tsx
+++ digital-sign-flow/src/components/SignPage/SignPage.tsx
@@ -7,7 +7,7 @@ function SignPage() {
   const [done, setDone] = useState(false);
 
   const fileUrl = useMemo(() => {
-    return `https://automation-project-server.onrender.com/uploads/${filename}`;
+    return `https://automation-project-server.onrender.com/uploads/${filename}.pdf`;
   }, [filename]);
 
   if (!filename) {
